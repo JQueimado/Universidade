ed05adba77c10071055f666e22a13a08623aafbe
JQueimado <l38176@alunos.uevora.pt>
2017-11-02 17:24:06 +0000

    Player Movement Limits Added

--- FMO/Scripts/Game/Player.gd
+++ FMO/Scripts/Game/Player.gd
@@ -3,6 +3,9 @@ extends Spatial;
 const Rotation_Flow=0.2;
 const Movement_Flow=0.1;
 
+const Max_Left=-2.736745;
+const Max_Right=2.736745;
+
 
 var player;
 var t;
@@ -16,10 +19,10 @@ func _ready():
 func _input(event):
 	t = player.get_transform();
 	
-	if event.is_action("Push"):
+	if event.is_action("Push") and t.origin[0]>=Max_Left:
 		t=t.translated(Vector3(-Movement_Flow,0,0));
 		
-	if event.is_action("Pull"):
+	if event.is_action("Pull") and t.origin[0]<=Max_Right:
 		t=t.translated(Vector3(Movement_Flow,0,0));
 	
 	if event.is_action("RotateLeft"):
@@ -28,6 +31,7 @@ func _input(event):
 	if event.is_action("RotateRight"):
 		t=t.rotated(Vector3(-1,0,0),Rotation_Flow);
 	
+	print(t.origin);
 	player.set_transform(t);
 
 
