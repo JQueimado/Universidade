97d2b99fc847b62e22b81f25a360069c4ff93f85
JQueimado <l38176@alunos.uevora.pt>
2017-11-02 15:45:14 +0000

    Added player controls

--- FMO/Scripts/Game/Player.gd
+++ FMO/Scripts/Game/Player.gd
@@ -1,10 +1,37 @@
 extends Spatial;
+
+const Rotation_Flow=0.2;
+const Movement_Flow=0.1;
+
+
+var player;
 var t;
+
 func _ready():
 	set_process(true);
+	set_process_input(true);
+	
+	player = get_node("MidPlayer");
+	
+func _input(event):
+	t = player.get_transform();
+	
+	if event.is_action("Push"):
+		t=t.translated(Vector3(-Movement_Flow,0,0));
+		
+	if event.is_action("Pull"):
+		t=t.translated(Vector3(Movement_Flow,0,0));
+	
+	if event.is_action("RotateLeft"):
+		t=t.rotated(Vector3(1,0,0),Rotation_Flow);
+		
+	if event.is_action("RotateRight"):
+		t=t.rotated(Vector3(-1,0,0),Rotation_Flow);
+	
+	player.set_transform(t);
+
 
 func _process(delta):
-	t = get_node("MidPlayer").get_transform();
-	t=t.rotated(Vector3(1,0,0),0.1);
-	get_node("MidPlayer").set_transform(t);
+	pass
+	
     
