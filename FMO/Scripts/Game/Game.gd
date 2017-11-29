49867b287f149dfa4798965561249c1d0326833d
JQueimado <l38176@alunos.uevora.pt>
2017-11-29 14:41:32 +0000

    Bug Fixes

--- FMO/Scripts/Game/Game.gd
+++ FMO/Scripts/Game/Game.gd
@@ -4,7 +4,9 @@ func _ready():
 	set_process_input(true);
 	
 func _input(event):
+	
 	if event.is_action_pressed("Debug_Restart_Button"):
 		get_tree().reload_current_scene();
+		
 	if event.is_action_pressed("back"):
 		get_tree().change_scene("res://Scenes/Menu/MenuScene.tscn");
