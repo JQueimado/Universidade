e8e678de54ed511ec236894417474ddbf4e63c07
JQueimado <l38176@alunos.uevora.pt>
2017-12-07 12:41:26 +0000

    SeverChanges

--- FMO/Scripts/Game/Game.gd
+++ FMO/Scripts/Game/Game.gd
@@ -7,6 +7,6 @@ func _input(event):
 	
 	if event.is_action_pressed("Debug_Restart_Button"):
 		get_tree().reload_current_scene();
-		
+	
 	if event.is_action_pressed("back"):
 		get_tree().change_scene("res://Scenes/Menu/MenuScene.tscn");
