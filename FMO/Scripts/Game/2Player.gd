479baee839dc5a792201295409a9b9c8cdade269
JQueimado <l38176@alunos.uevora.pt>
2017-12-13 16:10:09 +0000

    Client Changes

--- FMO/Scripts/Game/2Player.gd
+++ FMO/Scripts/Game/2Player.gd
@@ -10,10 +10,25 @@ func _ready():
 func _process(delta):
 	var sv = Server.receve();
 	if sv!=[]:
-		if str(sv[0])=="Move":
-			rotate()
+		if str(sv[0])=="1":
+			translate(1);
+		
+		if str(sv[0])=="2":
+			translate(-1);
+			
+		if str(sv[0])=="3":
+			rotate(-1);
+			
+		if str(sv[0])=="4":
+			rotate(1);
+		
 
-func rotate():
+func rotate(side):
 	var transform = Gk.get_transform();
-	transform=transform.rotated(Vector3(1,0,0),Movement_Flow);
+	transform=transform.rotated(Vector3(1,0,0),side*Movement_Flow);
+	Gk.set_transform(transform);
+	
+func translate(side):
+	var transform = Gk.get_transform();
+	transform=transform.translated(Vector3(side*Movement_Flow,0,0));
 	Gk.set_transform(transform);
--- FMO/Scripts/clientTest.gd
+++ FMO/Scripts/clientTest.gd
@@ -7,7 +7,15 @@ func _ready():
 	set_process_input(true);
 
 func _input(event):
-	if event.is_action("Debug_Server_Send_Msg"):
-		print("sent");
-		server.put_var("Move");
+	if event.is_action("ui_up"):
+		server.put_var(1);
+		
+	if event.is_action("ui_down"):
+		server.put_var(2);
+	
+	if event.is_action("ui_left"):
+		server.put_var(3);
+		
+	if event.is_action("ui_right"):
+		server.put_var(4);
 		
