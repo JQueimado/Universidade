3a7a0386a90f61296608b15214b86ab2cc88b9f2
AnaCarolinaEI <annabuchecha.97@gmail.com>
2017-12-20 00:33:09 +0000

    sound

--- FMO/Scripts/Menus/GameSetting.gd
+++ FMO/Scripts/Menus/GameSetting.gd
@@ -1,10 +1,10 @@
-extends VButtonArray
+extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process_input(true);
+
+
+func _input(event):
+	if event.is_action_pressed("back"):
+		get_tree().change_scene("res://Scenes/Menu/MenuScene.tscn");
