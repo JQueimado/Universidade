09f31665fde8476b540e7d3244834b3cd5f91104
AnaCarolinaEI <annabuchecha.97@gmail.com>
2018-05-03 12:59:31 +0100

    Só comecei isto.Foi uma experiencia.

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
+
 
 
 int main() {
     
-    return 0;    
+ FILE *inputFile;
+ inputFile = fopen("input_b.xpto", "r")  
+
+ //Read File into array
+
+    int process[size_File_line];
+    int i;
+
+ if (inputFile == NULL) {
+	printf("Erro: Does not exist\n");
+ }
+ 
+ for (i = 0; i < 16; i++){
+        fscanf(inputFile, "%d,", &process[i] );
+
+ fclose(myFile);
+ return 0;    
     
 }
