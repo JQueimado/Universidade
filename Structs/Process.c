852d658fc2595b8c83683f7629830dbb32b8be47
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-05-05 20:17:44 +0100

    Update Prosses.c

--- Structs/Process.c
+++ Structs/Process.c
@@ -1,16 +1,63 @@
-/*Class Instructions_Queue*/
-
 #include "Process.h"
 
+/*Includes*/
+#include <stdlib.h>
+
+/*Consts*/
+#define Instructions_Queue_STD_SIZE 200
+
+/**Class Instructions_Queue**/
+
+/*Define*/
 struct Instructions_Queue{
-
+	int header;
+	int footer;
 	int *script;
-
 };
 
-/*Class Process*/
+/*Constructors*/
+struct Instructions_Queue *new_Instructions_Queue(){
 
+	struct Instructions_Queue *temp = malloc(sizeof(Instructions_Queue));
+	temp->header = 0;
+	temp->footer = 0;
+	temp->script = int[Instructions_Queue_STD_SIZE];
+
+}
+
+struct Instructions_Queue *new_Instructions_Queue(int instructions[]){
+
+	struct Instructions_Queue *temp = malloc(sizeof(Instructions_Queue));
+	temp->header = 0;
+	temp->footer = 0;
+	temp->script = instructions;
+
+}
+
+/*Methods*/
+void is_empty(struct Instructions_Queue *queue){
+
+}
+
+void add(struct Instructions_Queue *queue, int item){
+
+}
+
+int remove(struct Instructions_Queue *queue){
+
+}
+
+int next(struct Instructions_Queue *queue){
+
+}
+
+/**Class Process**/
+
+/*Define*/
 struct Process{
 	int id;
+	int arrival_time;
+	int run_time;
 	struct Instructions_Queue *script;
+
 };
