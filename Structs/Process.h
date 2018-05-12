ba3dca676c9df9de3d2199a017be6ed221618930
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-05-12 20:16:58 +0100

    Process remake n2

--- Structs/Process.h
+++ Structs/Process.h
@@ -7,7 +7,7 @@
 /*Includes*/
 #include <stdlib.h>
 
-#define VAR_SIZE 10
+#define INST_POINTER 10
 #define MEM_SIZE 300
 
 /*Define*/
@@ -18,26 +18,29 @@ struct Process{
 	int arrival_time;
 	int inst_time;
 	int PC;
+	int inst_end;
 	
 	/*Memory*/
-	int VAR[VAR_SIZE];
-	int MEM[MEM_SIZE];
+	int MEM[MEM_SIZE]
 
 };
 
 /*Constructors*/
-struct Process *new_Process(int i, int arr_time, int inst[], int arr_size){
+struct Process *new_Process(int i, int arr_time, int inst[], int inst_size){
 	
 	struct Process *temp = malloc(sizeof(struct Process));
 
+	/*PCB*/
 	temp->id = i;
 	temp->arrival_time = arr_time;
 	temp->inst_time = 0;
-	temp->PC = 0;
+	temp->PC = INST_POINTER;
+	temp->inst_end = inst_size + INST_POINTER;
 
-	for (int i = 0; i < arr_size; ++i){
+	/*MEM*/
+	for (int i = 0; i < inst_size; ++i){
 		
-		temp->MEM[i] = inst[i];
+		temp->MEM[ i + INST_POINTER ] = inst[ i ];
 	
 	}
 
@@ -45,5 +48,29 @@ struct Process *new_Process(int i, int arr_time, int inst[], int arr_size){
 
 }
 
+/*Methods*/
+int get_var(struct Process *process , int var_name){
+
+	if ( (var_name < 0) || (var_name >= INST_POINTER) )
+		return -1;
+
+	return process->MEM[var_name];
+
+}
+
+int set_var(struct Process *process , int var_name , int val){
+
+	if ( (var_name < 0) || (var_name >= INST_POINTER) )
+		return -1;
+
+	process->MEM[var_name] = val;
+
+	return 0;
+
+}
+
+int get_inst(struct Process *process){
+	return process->MEM[process->PC];
+}
 
 #endif
