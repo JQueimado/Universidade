7af8afd23926d4c87cd231f35bf0575d0d7dc7cd
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-05-12 19:40:00 +0100

    Scheduling

--- Structs/Process.h
+++ Structs/Process.h
@@ -6,7 +6,6 @@
 
 /*Includes*/
 #include <stdlib.h>
-#include "Instruction_Queue.h"
 
 #define VAR_SIZE 10
 #define MEM_SIZE 300
@@ -17,6 +16,7 @@ struct Process{
 	/*PCB*/
 	int id;
 	int arrival_time;
+	int inst_time;
 	int PC;
 	
 	/*Memory*/
@@ -32,9 +32,10 @@ struct Process *new_Process(int i, int arr_time, int inst[], int arr_size){
 
 	temp->id = i;
 	temp->arrival_time = arr_time;
+	temp->inst_time = 0;
 	temp->PC = 0;
 
-	for (int i = 0; i < count; ++i){
+	for (int i = 0; i < arr_size; ++i){
 		
 		temp->MEM[i] = inst[i];
 	
--- main.c
+++ main.c
@@ -15,8 +15,11 @@
 
 /*Scheduling*/
 #define QUANTUM 4
+#define MAX_READY_SIZE 4
 
 /*Funcs*/
+
+/*Extract Input File*/
 int *extract(char *name){
 
 	FILE *inputFile;
@@ -45,20 +48,57 @@ int *extract(char *name){
 int main() {
     
     /*Lists*/
-    int *to_do_list;
+    struct Queue *to_do_list = new_Queue();
 
+    int ready_size = 0;
     struct Queue *ready = new_Queue();
+    
     struct Queue *blocked = new_Queue();
+    
     struct Process *run = NULL;
 
     int timer = 0;
 
     /*Extract file*/
-    to_do_list = extract("input_b.xpto");
+    extract("input_b.xpto");
 
     /*Processor loop*/
-    while(true){
+    while( !( is_empty( to_do_list ) && is_empty(ready) && run == NULL) ){
+        
+        /*Check Process Entry*/
+        struct Queue *temp = new_Queue();
+        struct Process *cur_pro = NULL;
 
+        while (!is_empty(to_do_list)){
+
+            cur_pro = dequeue(to_do_list);
+            
+            if (cur_pro->arrival_time <= timer){
+
+                if(ready_size >= MAX_READY_SIZE){
+                    
+                    enqueue(blocked , cur_pro);
+                
+                }else{
+                
+                    enqueue(ready , cur_pro);
+                
+                }
+
+            }else{
+
+                enqueue(temp, cur_pro);
+
+            }
+
+        }
+
+        while (!is_empty(temp)){
+
+            cur_pro = dequeue(temp);
+            enqueue(to_do_list , cur_pro);
+
+        }
 
 
     }
