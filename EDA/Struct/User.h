d4072a22b07b64f17f96077ad368198607029192
miguelt1 <l37508@alunos.uevora.pt>
2018-05-26 19:24:45 +0100

    asd

--- EDA/Struct/User.h
+++ EDA/Struct/User.h
@@ -22,10 +22,10 @@ int set_nick( struct User *user , char ni[] ){
 	bool hc_flag = false;
 	bool num_flag = false;
 
-	for (int i = 0; i < MAX_NICK_SIZE ; i++)
+	for (int i = 0; i <=MAX_NICK_SIZE ; i++)
 	{
 
-		if ( (ni[ i ] == '\0') && ( i != MAX_NICK_SIZE  ) )
+		if ( (ni[ i ] == '\0') && ( i == MAX_NICK_SIZE  ) )
 		{
 
 			return 1;
