e5ef3f15888b742a77e46be61ac1a80f96dc2d77
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-05-27 17:41:45 +0100

    Grafo changes

--- EDA/Struct/Grafos.h
+++ EDA/Struct/Grafos.h
@@ -6,6 +6,7 @@
 /*Includes*/
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**Consts**/
 #define SIZE 20
@@ -18,14 +19,23 @@
 #define GRAY 0
 #define BLACK -1
 
+/*
+
+Implementação de Grafos como uma listas de adjacências
+
+*/
 
 /****Class Vertice****/
 struct Vertice 
 {
 
-	int color;
 	int pos;
 	struct User *user;
+	
+	int color;
+	struct Vertice *p;
+	int d;
+	int f;
 
 };
 
@@ -88,6 +98,8 @@ struct Grafo *new_Grafo()
 }
 
 /*Methods*/
+
+/*Add Vertice*/
 int grafo_insert_vertice(struct Grafo *grafo , struct User *user)
 {	
 
@@ -112,6 +124,7 @@ int grafo_insert_vertice(struct Grafo *grafo , struct User *user)
 
 }
 
+/*Add Conection*/
 int grafo_insert_conection(struct Grafo *grafo , struct Vertice *v1 , struct Vertice *v2 )
 {
 
@@ -149,5 +162,98 @@ int grafo_insert_conection(struct Grafo *grafo , struct Vertice *v1 , struct Ver
 	return NORMAL;
 
 }
+
+/*Get Vertice from a given position*/
+struct Vertice *grafo_get_vertice_at( struct Grafo *grafo , int position )
+{
+
+	return grafo->nodes[ position ]->ver;	
+
+}
+
+/*Depth search*/
+struct Vertice *grafo_depth_visit(struct Grafo *grafo , struct Vertice *u , char *name[] )
+{
+
+	//time += 1;
+	//u->d = time;
+	u->color = GRAY;
+
+	struct Node *temp = grafo->nodes [ u->pos ]->next_node;
+
+	while ( temp != NULL)
+	{
+
+		struct Vertice *v = temp->ver;
+		char *v_name = v->user->nick;
+
+		if ( strcmp(v_name , name) != 0)
+		{
+			return v;
+		}
+
+		if (v->color == WHITE)
+		{	
+			
+			v->p = u;
+			
+			struct Vertice *check = grafo_depth_visit(grafo , v , name);
+			
+			if( check != NULL )
+			{
+
+				return check;
+
+			}
+
+		}
+
+		temp = temp->next_node;
+
+	}
+	
+	u->color = BLACK;
+	//time += 1;
+	//u->f = time;
+	return NULL;
+
+}
+
+struct User *grafo_depth_search( struct Grafo *grafo , char *name[] )
+{	
+
+	for (int i = 0; grafo->nodes[ i ] != NULL; ++i)
+	{
+		
+		grafo->nodes[ i ]->ver->color = WHITE;
+		grafo->nodes[ i ]->ver->p = NULL;
+	
+	}
+
+	//static int time = 0;
+
+	for (int i = 0; grafo->nodes[ i ] != NULL; ++i)
+	{
+		
+		if ( grafo->nodes[i]->ver->color == WHITE )
+		{
+
+			struct Vertice *check = grafo_depth_visit(grafo, grafo->nodes[ i ]->ver, name);
+			
+			if (check != NULL)
+			{
+				return check->user;
+			}
+
+		}
+	
+	}
+
+	return NULL;
+
+}
+
+
+/*Width search*/
 
 #endif
