ef0677fd210f707887803ea57bd0328c51f1f3c2
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-06-07 02:53:11 +0100

    ASVALAS

--- EDA/Test/trie_test.c
+++ EDA/Test/trie_test.c
@@ -4,7 +4,7 @@
 int main()
 {
 	
-
+/*
 	printf(" a pos de %c e %d\n", '1', pos('1'));
 	printf(" a pos de %c e %d\n", 'a', pos('a'));
 	printf(" a pos de %c e %d\n", 'A', pos('A'));
@@ -17,6 +17,14 @@ int main()
 	printf(" a pos de %d e %c\n", 1, to_char(1));
 	printf(" a pos de %d e %c\n", 0, to_char(0));
 	printf(" a pos de %d e %c\n", 10, to_char(10));
+*/
+
+	struct trie *t = trie_new();
+
+	trie_insert(t , "EUQUEROECONASsssss07");
+
+	if (trie_find(t, "EUQUEROECONASsssss07"))
+		puts("TA BOM CARALHO");
 
 	return 0;
 }
