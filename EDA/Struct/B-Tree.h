2b2fb37e3fc854e82d619e47934516c27ca086ac
JoÃo Queimado <l38176@alunos.uevora.pt>
2018-06-11 19:21:20 +0100

    Putas

--- EDA/Struct/B-Tree.h
+++ EDA/Struct/B-Tree.h
@@ -1,19 +1,67 @@
 
+#ifndef _BTree_
+#define _BTree_
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "User.h"
 
-#define 
+#define FILE_NAME "Cache.txt"
+#define M 32
 
-typedef struct Node
+/*Structs*/
+struct Node
 {
 	
 	int n;
-	struct User *ḱey[]
+	struct User *ḱey [M - 1];
+	struct Node *c[M];
+	bool leaf;
 
 };
 
-typedef struct B-Tree 
+struct BTree 
+{
+
+	struct Node *root;
+
+};
+
+/*Disk Acsses*/
+
+void Disk_Write(struct Node *x)
+{
+	FILE *file_pointer = fopen(FILE_NAME , "wb");
+
+	fwrite(x ,sizeof(struct Node), file_pointer);
+
+	fclose(file_pointer);
+
+
+}
+
+struct Node *Disk_Read()
 {
 
 
+}
 
-};
+/*Constructors*/
+struct B-Tree *new_BTree()
+{
+	struct BTree *tree = malloc(sizeof(BTree));
+	struct Node *x = malloc(sizeof(Node));
+
+	x->leaf = true;
+	x->n = 0;
+
+	Disk_Write(x);
+
+	tree->root = x;
+
+
+}
+
+#endif
---
+++ EDA/Test/B_Conas.c
@@ -0,0 +1,11 @@
+
+#include "../Struct/B-Tree.h"
+
+int main()
+{
+	
+	BTree *test_trie = new_BTree()
+	
+	return 0;
+
+}
