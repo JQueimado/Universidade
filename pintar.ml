c561c9ef727e8b36727ec78eba3bb1e26a19e938
JQueimado <queimadojp@gmail.com>
2019-01-03 18:56:40 +0000

    ja n funciona outravez

--- pintar.ml
+++ pintar.ml
@@ -1,22 +1,29 @@
-(*funcao que compara dois sectores do mesmo tamanho*)
+(*funcao que verifica se um elemento l já existe numa lista*)
 let rec find_equal = function l -> function [] -> false | s::sl -> if s=l then true else find_equal l sl;;
 
+(*função que calcula o tamanho de uma lista*)
 let rec lenght = function [] -> 0 | _::x -> 1+(lenght x);; 
 
+(*função que procura uma cor i numa palete*)
 let rec get_colour_at = function i -> function c::[] -> c | c::cl -> if i=0 then c else get_colour_at (i-1) cl ;;
 
+(*função que escolhe uma cor aleatoria de uma palete l*)
 let pick_colour l = get_colour_at (Random.int ( lenght l ) );;
 
+(*função que retira o elemento c de uma lista *)
 let rec pop_element = function c -> function [] -> [] | cc::cl -> if c = cc then cl else cc::(pop_element c cl) ;;
 
+(*função que procura uma cor aleatoria que não seja a c em uma palete l*)
 let find_different_colour c l = get_colour_at ( Random.int (lenght l)-1 ) ( pop_element c l );;
 
-(*funcao que cria um sector baseado em s com alteracao na posicao i*)
+(*funcao que cria um sector baseado em s com alteracao na posicao i com a palete l*)
 let rec sector l s i = match s with [] -> [] | x::ss -> if i=0 then ( ( find_different_colour x l )::( sector l ss (i-1) ) ) else ( x::( sector l ss (i-1) ) );;
 
+(*função que gera um sector aleatorio de tamanho n e com a palete l*)
 let rec rand_sec n l = if n=0 then [] else (get_colour_at (Random.int (lenght l)) l )::(rand_sec (n-1) l ) ;;
 
-(*funcao que pinta um circulo de varios sectores*)
+(*funcao recurciva que pinta n discos com m sectores com a palete k (l representa os sectores ja pintados)*)
 let rec pintar_rec n m k l = if m=0 then [] else match l with [] -> let x = rand_sec n k in x::(pintar_rec n (m-1) k [x]) | s::sl -> let x = sector k s (Random.int n) in if find_equal x l then x::(pintar_rec n m k l) else x::(pintar_rec n (m-1) k (x::l)) ;;
 
+(*função que pinta n discos com m sectores com a palete k*)
 let pintar n m k = pintar_rec n m k [];;
