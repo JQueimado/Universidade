67bc3e3a4e80e027faeec368598b6ede8c167869
JQueimado <queimadojp@gmail.com>
2019-01-03 11:19:55 +0000

    funciona

--- pintar.ml
+++ pintar.ml
@@ -12,9 +12,11 @@ let rec pop_element = function c -> function [] -> [] | cc::cl -> if c = cc then
 let find_different_colour c l = get_colour_at ( Random.int (lenght l)-1 ) ( pop_element c l );;
 
 (*funcao que cria um sector baseado em s com alteracao na posicao i*)
-let rec sector l s i = match s with [] -> [] | x::ss -> if i=1 then ( ( find_different_colour x l )::( sector l ss (i-1) ) ) else ( x::( sector l ss (i-1) ) );;
+let rec sector l s i = match s with [] -> [] | x::ss -> if i=0 then ( ( find_different_colour x l )::( sector l ss (i-1) ) ) else ( x::( sector l ss (i-1) ) );;
+
+let rec rand_sec n l = if n=0 then [] else (get_colour_at (Random.int (lenght l)) l )::(rand_sec (n-1) l ) ;;
 
 (*funcao que pinta um circulo de varios sectores*)
 let rec pintar_rec n m k s = if m=0 then [] else let x = ( sector k s (Random.int n ) ) in x::( pintar_rec n (m-1) k x );;
 
-let pintar n m k = pintar_rec n m k [ get_colour_at ( Random.int (lenght k) ) ; get_colour_at ( Random.int (lenght k) ) ];;
+let pintar n m k = pintar_rec n m k (rand_sec n k);;
