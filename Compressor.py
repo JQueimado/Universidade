e1f2e8f67ec7442f8b905b371d17523fca8e47ac
JQueimado <queimadojp@gmail.com>
2019-01-07 18:48:29 +0000

    fiz cenas

--- Compressor.py
+++ Compressor.py
@@ -1,11 +1,81 @@
 
 # const #
 
-FILE_NAME = "dogo_bin.pbm"
+FILE_TREM = ".best"
 
 def compress (f):
-    pass
+    
+    d = {"0":0 , "1":1}
+    i = 2
+
+    p = ''
+
+    c = ''
+
+    out = []
+
+    for c in f:
+        
+        pc = p + c
+
+        if pc in d:
+            
+            p = pc
+        
+        else:
+
+            d[pc] = i
+            i += 1
+
+            out.append( d.get( p ) )
+
+            p = c
+
+    return out
+
+def write_file (fo, s, fn):
+
+    f = open(fn, 'w+')
+    f.write(s)
+
+    for c in fo:
+        f.write(' '+ str(c) )
+
+    f.close()
+
+def read_file(fn):
+    
+    f = open (fn, "r")
+
+    t = f.readline()[:-1]
+
+    s = f.readline()[:-1]
+
+    ff = []
+
+    for l in f:
+        for c in l:
+            if c != ' ' and c !='\n':
+                ff.append(c)
+
+    f.close()
+
+    return ff , t , s
 
 if __name__ == "__main__":
-    pass
+    
+    fn = input("Enter image file name:")
+
+    ff,_,s = read_file(fn)
+
+    fo = fn[:-4]+FILE_TREM
+
+    write_file( compress ( ff ), s, fo )
+
+    print("Done.")
+
+
+
+
+
 
---
+++ Descompressor.py
@@ -0,0 +1,31 @@
+FILE_NAME = "j.best"
+
+FILE_TERM = ".pmb"
+
+FILE_OUTPUT = FILE_NAME[:-5]+FILE_TERM
+
+def descompress (f):
+
+    out = lista
+
+    p = chr(f.pop(0))
+
+    out.write(p)
+
+    for a in f:
+
+        if a in d
+
+    pass
+
+def write_file (fo, s, t):
+
+    pass
+
+def read_file(fn):
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
