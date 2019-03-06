d4923b1b0ac862e0dff4cc1c4caf9b1015d7be1c
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-03-06 17:50:54 +0000

    coments

--- C/C1.c
+++ C/C1.c
@@ -4,16 +4,27 @@
 
 /* limits */
 
-#define NAME_LIMIT 20
-#define TEAM_LIMIT 20
+#define NAME_LIMIT 20 /* name size limiter */
+#define TEAM_LIMIT 20 /* number of times limiter */
 
 /* score */
 
-#define W 3
-#define D 1
-#define L 0
+#define W 3 /* Winner points */
+#define D 1 /* Draw points */
+#define L 0 /* Loser points (useless) */
 
 /***** team *****/
+
+/* 
+Struct team:
+    -name           = team name
+    -score          = current team score on the tournament
+    -goals          = goals scored by the team
+    -goals_against  = goals scored against the team
+    -wins           = current number of wins
+    -draws          = current number of draws
+    -losses         = current number of losses  
+*/
 
 struct team
 {
@@ -31,7 +42,10 @@ struct team
 
 };
 
-/* construct */
+/* 
+Construct 
+-alocates memory for a new struct team and sets all values except name to 0 (creates object of type team)
+*/
 
 struct team *team_const( char *name )
 {
@@ -55,6 +69,13 @@ struct team *team_const( char *name )
 
 /****************/
 
+/*** program ***/
+
+/*
+Get team function
+-linear search algorithm to find a team type "object" in a linear array with a given team name
+*/
+
 struct team *get_team(struct team *tabel[], int s, char *name)
 {
 
@@ -76,7 +97,7 @@ struct team *get_team(struct team *tabel[], int s, char *name)
 
 }
 
-/* eval */
+/* Eval function*/
 
 struct team *eval(struct team *teams[], int s)
 {
@@ -126,11 +147,17 @@ int main ( void )
     short n_teams;
     short n_match;
 
-    /* read line 1*/
+    /* 
+    Read line 1
+    -Reads 2 short type values that represent the number of teams and number of matches
+    */
 
     scanf("%hu %hu", &n_teams, &n_match);
 
-    /* read teams */
+    /* 
+    Read teams 
+    -Reads teams names from the user and creates a "object" teams with the input information
+    */
 
     struct team *teams[n_teams];
 
@@ -145,7 +172,10 @@ int main ( void )
 
     }
 
-    /* read games */
+    /* 
+    Read matches 
+    -reads input from user and processes who won the match
+    */
 
     char *name1 = malloc(NAME_LIMIT + 1 );
     char *name2 = malloc(NAME_LIMIT + 1 );
@@ -160,6 +190,8 @@ int main ( void )
         struct team *team1 = get_team(teams, n_teams, name1);
         struct team *team2 = get_team(teams, n_teams, name2);
 
+        /* if it cant find 0ne of teams or a team had a match against it self the program ignores the match */
+
         if (team1 == NULL || team2 == NULL || team1 == team2)
         {
 
@@ -167,14 +199,20 @@ int main ( void )
 
         }
 
+        /* goals processing */
+
         team1->goals += goal1;
         team1->goals_against += goal2;
 
         team2->goals += goal2;
         team2->goals_against += goal1;
 
+        /* match processing */
+
         if ( goal1 > goal2)
         {
+
+            /* team1 wins */
 
             team1->score += W;
             team1->wins += 1;
@@ -185,6 +223,8 @@ int main ( void )
         else if (goal1 < goal2)
         {
            
+            /* team2 wins */
+
             team2->score += W;
             team2->wins += 1;
             
@@ -193,6 +233,8 @@ int main ( void )
         }
         else
         {
+
+            /* no team wins */
 
             team1->score += D;
             team2->score += D;
@@ -204,8 +246,10 @@ int main ( void )
         
     }
 
-    /* Eval */
-
+    /* 
+    Eval 
+    -Cheks for winners 
+    */
 
     struct team *top = eval(teams, n_teams);
 
@@ -218,3 +262,5 @@ int main ( void )
         printf("a vencedora foi %s, com %d ponto(s)\nganhou %d jogo(s), empatou %d jogo(s) e perdeu %d jogo(s)\nmarcou %d golo(s) e sofreu %d golo(s)\n", top->name, top->score, top->wins, top->draws, top->losses, top->goals, top->goals_against);
 
 }
+
+/****************/
