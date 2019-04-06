196afff714928edd487834ee727000919aa98743
JQueimado <jpqueimado@gmail.com>
2019-04-06 16:10:59 +0100

    Add gerador.c

---
+++ G/gerador.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main( int argc, char const *argv[] )
+{
+    int id;
+    unsigned int t1;
+    unsigned int t2;
+    int rendimento;
+    
+
+    return 0;
+}
