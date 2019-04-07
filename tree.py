c719be27077bbcaa0f11893f4ba5325bdb0598ee
massudo <mfgluis1997@gmail.com>
2019-04-07 22:11:27 +0100

    added insert to tree

--- tree.py
+++ tree.py
@@ -1,7 +1,8 @@
 class Node:
 
-    def __init__(self, value):
+    def __init__(self, value, tipo):
         self.value = value
+        self.type = tipo
         self.left_child = None
         self.right_child = None
 
@@ -9,15 +10,24 @@ class Tree:
     def __init__(self):
         self.root = None
     
-    def insert(self, value):
+    def insert(self, value, tipo):
         if self.root == None:
-            self.root = node(value)
-        else:
-            self._insert(value, self.root)
+            self.root = Node(value, tipo)
 
-    def _insert(self, value, curr_node):
+        elif self.root.left_child == None:
+            self.root.left_child.value = value
+            self.root.left_child.type = tipo
         
+        elif self.root.right_child == None:
+            self.root.right_child.value = value
+            self.root.right_child.type = tipo
+        
+        else:
+            self.root.left_child.insert(value, tipo)
+            self.root.right_child.insert(value, tipo)
+        
+    def printTree(self):
+        if self.root:
+            self.root.printTree()
+            print("")
 
-
-    def checkValue( curr_node, value)
-        if 
