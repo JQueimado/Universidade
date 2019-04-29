35e24e5fdb6213c5cad66d82f1a654022f63b4f2
AnaCarolinaEI <annabuchecha.97@gmail.com>
2019-04-29 18:00:50 +0100

    bug fix

--- SO/main.c
+++ SO/main.c
@@ -356,7 +356,9 @@ int main() {
 
             run = CPU(run , MEM);
 
-            printf("%d : %d\n" , count , run->id );
+            printf("%d : P[%d]\n" , count , run->id );
+		
+			
 
             /*Check if process is waiting for a message*/
             if (run->block_time != -1){
