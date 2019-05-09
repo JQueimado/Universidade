2a907f4d6b5e73bd3ab38a4a39d6470b67b0de6e
JQueimado <jpqueimado@gmail.com>
2019-05-09 23:36:48 +0100

    merge_conflit

--- SO/Structs/Pre_Process.h
+++ SO/Structs/Pre_Process.h
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-/**Class Pre_Process**/
-
-/*Comflit avoider*/
-#ifndef _Pre_Process_
-#define _Pre_Process_
-
-/*Includes*/
-#include <stdlib.h>
-
-struct Pre_Process{
-	int arrival;
-	int file_pos;
-	int size;
-	int done;
-};
-
-struct Pre_Process *new_Pre_Process(int a , int fp , int s ){
-
-	struct Pre_Process *temp = malloc(sizeof(struct Pre_Process));
-
-	temp->arrival = a;
-	temp->file_pos = fp;
-	temp->size = s;
-	temp->done = 0;
-
-	return temp;
-
-}
-
-=======
 /**Class Pre_Process**/
 #ifndef _Pre_Process_
 #define _Pre_Process_
@@ -54,5 +23,4 @@ typedef struct Pre_Process{
 */
 Pre_Process *new_Pre_Process(int a , int fp , int s );
 
->>>>>>> 2e6249faf9be84aa91dba7e62866ad8ffd030373
 #endif
