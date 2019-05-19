6e5103c152b6b717f2e2635fb140f668db166dd2
JQueimado <jpqueimado@gmail.com>
2019-05-19 21:13:46 +0100

    best fit

--- SO/Structs/BestFit.c
+++ SO/Structs/BestFit.c
@@ -1,15 +1,16 @@
 #include "MemoryManager.h"
 /* Best Fit implementation */
-#define MAX_FRAME 150
-#define 
+#define MEM_SIZE 300
+#define MAX_FRAME MEM_SIZE/2
 
 int find_pos(Process** processes, int p_size,  int* Memory)
 {
+    /* find empty frames */
     int frame_pointer[MAX_FRAME];
     int frame_size[MAX_FRAME];
     for(int i = 0; i < p_size; i++)
     {
         Process* curr = processes[i];
-
+        
     }
 }
