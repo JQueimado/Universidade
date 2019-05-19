a11883d79ead2580a71c7a63e9f27df77f4719be
JQueimado <jpqueimado@gmail.com>
2019-05-19 15:19:01 +0100

    bestfit

---
+++ SO/Structs/BestFit.c
@@ -0,0 +1,15 @@
+#include "MemoryManager.h"
+/* Best Fit implementation */
+#define MAX_FRAME 150
+#define 
+
+int find_pos(Process** processes, int p_size,  int* Memory)
+{
+    int frame_pointer[MAX_FRAME];
+    int frame_size[MAX_FRAME];
+    for(int i = 0; i < p_size; i++)
+    {
+        Process* curr = processes[i];
+
+    }
+}
---
+++ SO/Structs/MemoryManager.h
@@ -0,0 +1,4 @@
+/* Memory Manager */
+#include "Process.h"
+
+int find_pos(Process** processes, int p_size, int* Memory);
