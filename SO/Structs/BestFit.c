acab85004df212deb033f7036edbb8768f8fac16
JQueimado <jpqueimado@gmail.com>
2019-06-05 01:22:08 +0100

    BestFit

--- SO/Structs/BestFit.c
+++ SO/Structs/BestFit.c
@@ -3,14 +3,103 @@
 #define MEM_SIZE 300
 #define MAX_FRAME MEM_SIZE/2
 
+/* New Frame */
+Frame* new_frame(short init, short end)
+{
+    Frame* temp = malloc(sizeof(Frame));
+    temp->init = init;
+    temp->end = end;
+    temp->next = NULL;
+    return temp;
+}
+
+/* Add Frame to list */
+Frame* add_to_frame_list(Frame* head, Frame* item )
+{
+    /* if empty */
+    if ( head == NULL )
+        return item;
+
+    /* if head */
+    if(head->init < item->init)
+    {
+        item->next = head;
+        return item;
+    }
+
+    /* if second is empty */
+    if( head->next == NULL )
+    {
+        head->next = item;
+        return head;
+    }
+
+    /* find place */
+    Frame* header = head;
+    while (head->next->init < item->init)
+    {
+        head = head->next;
+        
+        if( head->next == NULL)
+        {
+            head->next = item;
+            return header;
+        }
+    }
+    item->next = head->next;
+    head->next = item;
+    return header;
+}
+
+/* Find pos */
 int find_pos(Process** processes, int p_size,  int* Memory)
 {
-    /* find empty frames */
-    int frame_pointer[MAX_FRAME];
-    int frame_size[MAX_FRAME];
+    Frame* head = NULL;
+
+    /* Project ocupied memory in Frames */
     for(int i = 0; i < p_size; i++)
     {
         Process* curr = processes[i];
-        
+        if(!curr->in_memory)
+            continue;
+        head = add_to_frame_list(head, new_frame(curr->process_pointer, curr->process_pointer));
     }
+
+    /* if no process */
+    if(head == NULL)
+        return 0;
+
+    /* if 1 process */
+    if(head->next == NULL)
+        return head->end + 1;
+
+    /* if > 1 */
+    int diference;
+    Frame* seleced_frame;
+
+    while(head != NULL)
+    {
+        /* Calc free Space */
+        int free_space = head->next->init - head->end - 1;
+        
+        /* Check if fit */
+        if( free_space > p_size )
+        {
+            /* Calc free Space after allocasion */
+            free_space = free_space - p_size;
+
+            /* Check for Best feet */
+            if( diference > free_space )
+            {
+                diference = free_space;
+                seleced_frame = head;
+            }
+        }
+        
+        /* move list */
+        head = head->next;
+    }
+
+    return seleced_frame->end + 1;
+
 }
--- SO/Structs/MemoryManager.h
+++ SO/Structs/MemoryManager.h
@@ -1,4 +1,13 @@
 /* Memory Manager */
 #include "Process.h"
 
+typedef struct Frame
+{
+    struct Frame *next;
+    short init;
+    short end;
+}Frame;
+
+Frame* new_frame(short init, short end);
+
 int find_pos(Process** processes, int p_size, int* Memory);
