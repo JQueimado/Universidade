9c52602c13e6be6dca808b1b3d7084638dfa7427
JQueimado <jpqueimado@gmail.com>
2019-06-22 13:59:34 +0100

    moved_distra

---
+++ upgrade/Dijkstra.h
@@ -0,0 +1,11 @@
+#ifndef Dijkstra
+#define Dijkstra
+
+#include <malloc.h>
+#include "hashtable_aeroporto.h"
+
+#define INF -1
+
+aeroportos* dijkstra( int fd, char* init_code, char* final );
+
+#endif // _Grafo_
