a322b98601d0954f51010e19e120e33969353963
JQueimado <jpqueimado@gmail.com>
2019-07-02 19:04:08 +0100

    nada

---
+++ 4inline_Python/src/main-alfabeta.py
@@ -0,0 +1,15 @@
+from alfabetacut import *
+from four_in_line import *
+
+EXPAN = 7
+
+inf = 100000000
+if __name__ == "__main__":
+    istate = State()
+
+    tree = AlfaBetaCut( istate )
+    val , l = tree.minmax( EXPAN, P1 )
+    for i in l:
+        print(" result: ")
+        i.get_state().show()
+    print( "MINMAX value: " + str( val ) )
