301d3392df8412d11ce936f03dfc05fab581beda
JQueimado <jpqueimado@gmail.com>
2020-06-16 19:47:50 +0100

    setup_T2

--- T2/Trab2/Trab2/src/main/java/Backend/BackendServlet.java
+++ T2/Trab2/Trab2/src/main/java/Backend/BackendServlet.java
@@ -3,16 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Trab2;
+package Backend;
 
 /**
  *
  * @author joao
  */
-public class Main {
-
-    public static void main(String[] args) {
-        System.err.println("IT Works");    
-    }
+public class BackendServlet {
     
 }
