0deaebdaaad9bd4961ba912276cd73b471b2e78c
JQueimado <jpqueimado@gmail.com>
2020-06-27 17:34:10 +0100

    Database Fixes

--- T2/Trabalho2-V3.0/demo/src/main/java/com/example/demo/DB/Entities/RegistryEntity.java
+++ T2/Trabalho2-V3.0/demo/src/main/java/com/example/demo/DB/Entities/RegistryEntity.java
@@ -6,34 +6,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="registry")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Registry {
+public class RegistryEntity {
     
-    @Registry_Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long registry_id;
+    private long id;
     
-    @Level
+    @Column
     private String level; 
-
-    @OneToMany(fetch = FetchType.EAGER)
-    @JoinTable( 
-        name = "super_registry", 
-        joinColumns = @JoinColumn(
-          name = "registry_id", referencedColumnName = "id"), 
-        inverseJoinColumns = @JoinColumn(
-          name = "super_id", referencedColumnName = "id")) 
-    private Collection<Super_RegistryEntity> ids;
 
     // Constructors
 
@@ -66,14 +51,6 @@ public class Registry {
     public void setLevel(String level) {
 
         this.level = level;
-    }
-
-     public Collection<Super_RegistryEntity> getIds() {
-        return ids;
-    }
-
-    public void setIds(Collection<Super_RegistryEntity> ids) {
-        this.ids = ids;
     }
 
 }
---
+++ T2/Trabalho2-V3.0/demo/src/main/java/com/example/demo/DB/Entities/SuperMarketEntity.java
@@ -0,0 +1,45 @@
+package com.example.demo.DB.Entities;
+
+import com.example.demo.UserDB.Entities.RoleEntity;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="super")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperMarketEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    @Column
+    private String name; 
+    
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinTable( 
+        name = "super_registrys", 
+        joinColumns = @JoinColumn(
+          name = "super_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(
+          name = "registry_id", referencedColumnName = "id")) 
+    private Collection<RegistryEntity> registries;
+    
+}
