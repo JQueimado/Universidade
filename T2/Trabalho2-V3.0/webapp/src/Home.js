862239ccaf06ee55cf12824993761bd2a4e20c4c
JQueimado <jpqueimado@gmail.com>
2020-07-06 18:09:45 +0100

    added webapp

---
+++ T2/Trabalho2-V3.0/webapp/src/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello Shoppers!</h2>
+        <p>This app is a new game changer to supermarket shoppers in your local town. Gone are the days that consumers feared going shopping for the necessary itens,and comming home infect with the new virus. </p>
+ 
+        <p>The app informs every consumer of the degree/level of ocupation that a certain supermarket has. This way,  the problem of overcrowd can be avoided. The levels are in 4 categories:</p>
+
+        <ol>
+          <li>Empty or with minimum ocupation</li>
+          <li>With people, but with enouth space</li>
+          <li>overcrowded</li>
+          <li>overcrowded, with a big queue</li>
+        </ol>
+
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ T2/Trabalho2-V3.0/webapp/src/Registry.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Registry extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Ocupation Registry</h2>
+
+        <button class="button button1">Create Registry</button>
+        <button class="button button1">Show Registry</button>
+      </div>
+    );
+  }
+}
+ 
+export default Registry;
